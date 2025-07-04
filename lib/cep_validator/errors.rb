c8a4887e78db0a6c2d293cc0f234717d30ae1f2d
Carlos Henrique <hencarlosdv@gmail.com>
2025-07-04 09:49:07 -0300

    feat: add errors

---
+++ lib/cep_validator/errors.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module CepValidator
+  class InvalidFormatError < StandardError; end
+  class NotFoundError < StandardError; end
+  class ApiError < StandardError; end
+end
