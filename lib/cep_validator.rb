b92e11fb27e2eb2c5b9be36aab915ba169222656
Carlos Henrique <hencarlosdv@gmail.com>
2025-07-04 09:53:06 -0300

    ref: remove unused Client class that used HTTParty

--- lib/cep_validator.rb
+++ lib/cep_validator.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "cep_validator/version"
-require_relative "cep_validator/client"
 
 module CepValidator
   class Error < StandardError; end
-
-  def self.valid?(cep)
-    Client.valid?(cep)
-  end
 end
