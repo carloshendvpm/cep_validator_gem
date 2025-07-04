437b309c517b14287af83cb2a414f654db8436d2
Carlos Henrique <hencarlosdv@gmail.com>
2025-07-04 09:54:52 -0300

    test: update tests

--- spec/cep_validator_spec.rb
+++ spec/cep_validator_spec.rb
@@ -1,19 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe CepValidator do
-  it "has a version number" do
-    expect(CepValidator::VERSION).not_to be nil
+  describe ".valid?" do
+    it "returns true for a valid CEP" do
+      expect(CepValidator.valid?("01001-000")).to eq(true)
+    end
+
+    it "returns false for invalid format" do
+      expect(CepValidator.valid?("abc")).to eq(false)
+    end
+
+    it "returns false for not found CEP" do
+      expect(CepValidator.valid?("00000-000")).to eq(false)
+    end
   end
 
-  it "returns true for a valid CEP" do
-    expect(CepValidator.valid?("01001-000")).to eq(true)
-  end
+  describe ".fetch" do
+    it "returns a CepValidator::Address for a valid CEP" do
+      address = CepValidator.fetch("01001-000")
+      expect(address).to be_a(CepValidator::Address)
+      expect(address.cep).to eq("01001-000")
+    end
 
-  it "returns false for an invalid CEP" do
-    expect(CepValidator.valid?("00000-000")).to eq(false)
-  end
+    it "raises InvalidFormatError for malformed CEP" do
+      expect { CepValidator.fetch("12") }.to raise_error(CepValidator::InvalidFormatError)
+    end
 
-  it "returns false for a malformed CEP" do
-    expect(CepValidator.valid?("abc123")).to eq(false)
+    it "raises NotFoundError for nonexistent CEP" do
+      expect { CepValidator.fetch("00000-000") }.to raise_error(CepValidator::NotFoundError)
+    end
   end
 end
