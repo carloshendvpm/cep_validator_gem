41157e024f1a476ca8cd8b2912d4c1291cd8f0f2
Carlos Henrique <hencarlosdv@gmail.com>
2025-07-04 09:54:46 -0300

    ref: update main module to integrate Validator and Fetcher

--- lib/cep_validator.rb
+++ lib/cep_validator.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
 require_relative "cep_validator/version"
+require_relative "cep_validator/errors"
+require_relative "cep_validator/validator"
+require_relative "cep_validator/address"
+require_relative "cep_validator/fetcher"
 
 module CepValidator
-  class Error < StandardError; end
+  def self.valid?(cep)
+    Validator.new(cep).validate
+    begin
+      Fetcher.new(cep).fetch
+      true
+    rescue NotFoundError
+      false
+    end
+  rescue InvalidFormatError
+    false
+  end
+
+  def self.fetch(cep)
+    Fetcher.new(cep).fetch
+  end
 end
